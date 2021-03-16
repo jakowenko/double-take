feca83ff47c5486ee7fcd5de488480449a7f142b
David Jakowenko <jakowenko@gmail.com>
2021-03-16 02:31:17 -0400

    Overall cleanup of how manual events are handled and validated

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const { validate } = require('../util/validate.util');
 const recognize = require('../controllers/recognize.controller');
 const train = require('../controllers/train.controller');
-const { manual } = require('../util/events.util');
-const middleware = require('../util/middleware.util');
+const validators = require('../util/validators.util');
 
 const router = express.Router();
 
-router.post('/recognize', validate(middleware.recognize()), recognize.start);
-router.get('/recognize', validate(middleware.recognize(true)), manual, recognize.start);
+router.post('/recognize', validate(validators.recognize({ post: true })), recognize.start);
+router.get('/recognize', validate(validators.recognize({ get: true })), recognize.start);
 
 router.get('/train/add/:name', train.init);
 router.get('/train/remove/:name', train.delete);
-router.get('/train/:camera/:name', validate(middleware.train()), train.camera);
+router.get('/train/:camera/:name', validate(validators.train()), train.camera);
 
 module.exports = router;
