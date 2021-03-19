1282076c2663b7fae09541bc8cd46fc417517564
David Jakowenko <jakowenko@gmail.com>
2021-03-19 02:57:27 -0400

    respond util for api

---
+++ src/util/respond.util.js
@@ -0,0 +1,35 @@
+const { BAD_REQUEST } = require('../constants/http-status');
+
+module.exports.respond = (err, res) => {
+  const status = err && err.status ? err.status : BAD_REQUEST;
+  const firstChar = parseInt(status.toString().charAt(0), 10);
+  let message = { sucess: true };
+
+  if (firstChar === 4 || firstChar === 5) {
+    message = { error: err.message };
+  }
+
+  if (firstChar === 2) {
+    message = err.message ? err.message : message;
+  }
+
+  return res.status(status).json(message);
+};
+
+module.exports.HTTPSuccess = (status, message) => {
+  return { status, message };
+};
+
+module.exports.HTTPError = (status, message) => {
+  if (typeof message === 'string') {
+    const error = new Error(message);
+    error.status = status;
+    return error;
+  }
+  if (typeof message === 'object') {
+    return { status, message };
+  }
+
+  return { status, message: true };
+};
+// this.HTTPError.prototype = Object.create(Error.prototype);
