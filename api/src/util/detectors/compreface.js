f392a05dea5524ffd78aa97694dc54ba73efa402
David Jakowenko <jakowenko@gmail.com>
2021-05-17 22:13:00 -0400

    better catching of compreface response for errors

--- api/src/util/detectors/compreface.js
+++ api/src/util/detectors/compreface.js
@@ -64,7 +64,7 @@ module.exports.remove = ({ name }) => {
 };
 
 module.exports.normalize = ({ data }) => {
-  if (data.code === 28) return [];
+  if (data.code === 28 || data.sucess === false) return [];
   const { MIN_AREA_MATCH } = OBJECTS.FACE;
   const normalized = data.result.map((obj) => {
     const [face] = obj.subjects;
