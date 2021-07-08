2e4f58c26a6f1b9d88ecf78cb7ce42456c1d546e
David Jakowenko <jakowenko@gmail.com>
2021-07-08 12:08:42 -0400

    refactor(api:config): refactored config with support for override file

--- api/src/constants/config.js
+++ api/src/constants/config.js
@@ -1,10 +1,12 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const _ = require('lodash');
-const defaults = require('./defaults');
+const DEFAULTS = require('./defaults');
+const { core: SYSTEM_CORE, local: SYSTEM_LOCAL } = require('./system');
 const { version } = require('../../package.json');
 
-let config = false;
+let CONFIG = false;
+let OVERRIDE = false;
 
 const customizer = (objValue, srcValue) => {
   if (_.isNull(srcValue)) {
@@ -12,21 +14,53 @@ const customizer = (objValue, srcValue) => {
   }
 };
 
-module.exports = () => {
-  if (config) return config;
-
-  config = {};
+const loadYaml = (file) => {
   try {
-    config = { ...yaml.load(fs.readFileSync(`${__dirname}/../../../config.yml`, 'utf8')) };
-    // eslint-disable-next-line no-empty
-  } catch (error) {}
-
-  if (!config.notify || !config.notify.gotify) {
-    delete defaults.notify.gotify;
+    return yaml.load(fs.readFileSync(file, 'utf8'));
+  } catch (error) {
+    return error;
   }
-  config = _.isEmpty(config) ? defaults : _.mergeWith(defaults, config, customizer);
-  config.storage = { path: './.storage', ...config.storage };
-  config.version = version;
-  config = _(config).toPairs().sortBy(0).fromPairs().value();
-  return config;
+};
+
+const setup = (file, message) => {
+  if (!fs.existsSync(SYSTEM_CORE.storage.config.path))
+    fs.mkdirSync(SYSTEM_CORE.storage.config.path, { recursive: true });
+  fs.writeFileSync(`${SYSTEM_CORE.storage.config.path}/${file}`, message);
+};
+
+module.exports = () => {
+  if (CONFIG) return CONFIG;
+
+  CONFIG = {};
+  OVERRIDE = {};
+
+  const isLegacyPath = fs.existsSync('./config.yml');
+  if (isLegacyPath)
+    console.warn(
+      'config.yml file loaded from legacy path, this will be removed in a future update'
+    );
+
+  const configData = loadYaml(
+    isLegacyPath ? './config.yml' : `${SYSTEM_CORE.storage.config.path}/config.yml`
+  );
+  if (configData && configData.code === 'ENOENT') setup('config.yml', '# Double Take\n');
+  else CONFIG = { ...configData };
+
+  const overrideData =
+    process.env.NODE_ENV === 'local'
+      ? loadYaml(`${SYSTEM_CORE.storage.config.path}/config.override.yml`)
+      : false;
+  if (overrideData && (overrideData.code === 'ENOENT' || _.isEmpty(overrideData))) {
+    OVERRIDE = { ...SYSTEM_LOCAL };
+    setup('config.override.yml', `# Double Take\n\n${yaml.dump(SYSTEM_LOCAL)}`);
+  } else OVERRIDE = { ...overrideData };
+
+  if (!CONFIG.notify || !CONFIG.notify.gotify) delete DEFAULTS.notify.gotify;
+
+  CONFIG = _.isEmpty(CONFIG) ? DEFAULTS : _.mergeWith(DEFAULTS, CONFIG, customizer);
+  CONFIG = _.mergeWith(CONFIG, SYSTEM_CORE);
+  if (process.env.NODE_ENV === 'local') CONFIG = _.mergeWith(CONFIG, OVERRIDE, customizer);
+  CONFIG.version = version;
+  CONFIG = _(CONFIG).toPairs().sortBy(0).fromPairs().value();
+  return CONFIG;
 };
