f2184fa5f12eaa2139e607eda1bd9446d4dc6763
David Jakowenko <jakowenko@gmail.com>
2021-07-08 02:21:15 -0400

    refactor: read config file location from constants path

--- api/src/controllers/config.controller.js
+++ api/src/controllers/config.controller.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const config = require('../constants/config');
+const { STORAGE } = require('../constants');
 const { respond, HTTPSuccess /* , HTTPError */ } = require('../util/respond.util');
 const { OK } = require('../constants/http-status');
 
@@ -7,7 +8,7 @@ module.exports.get = async (req, res) => {
   try {
     const { format } = req.query;
     const output =
-      format === 'yaml' ? fs.readFileSync(`${__dirname}/../../../config.yml`, 'utf8') : config();
+      format === 'yaml' ? fs.readFileSync(`${STORAGE.CONFIG.PATH}/config.yml`, 'utf8') : config();
     res.status(OK).json(output);
   } catch (error) {
     respond(error, res);
@@ -17,7 +18,7 @@ module.exports.get = async (req, res) => {
 module.exports.patch = async (req, res) => {
   try {
     const { code } = req.body;
-    fs.writeFileSync(`${__dirname}/../../../config.yml`, code);
+    fs.writeFileSync(`${STORAGE.CONFIG.PATH}/config.yml`, code);
     respond(HTTPSuccess(OK, req.body), res);
   } catch (error) {
     respond(error, res);
