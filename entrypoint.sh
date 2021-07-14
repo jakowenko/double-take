f26ad0c59bc86ac847c4d7e29891a289b0dc27d1
David Jakowenko <jakowenko@gmail.com>
2021-07-14 02:30:49 -0400

    style: single command to multiple for readability

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir -p ./.storage/config
-[ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml && \
+[ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml
 nodemon -e yml --watch ./.storage/config --watch ./config.yml -q api/server.js
