65894af1fc15fded9afa60a61bea859cedc36bc2
David Jakowenko <jakowenko@gmail.com>
2021-07-14 02:32:19 -0400

    chore: require constants first

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 require('./src/util/logger.util').init();
 const http = require('http');
+const { SERVER } = require('./src/constants');
 const { version } = require('./package.json');
 const mqtt = require('./src/util/mqtt.util');
 const { app } = require('./src/app');
@@ -7,7 +8,6 @@ const storage = require('./src/util/storage.util');
 const database = require('./src/util/db.util');
 const config = require('./src/constants/config');
 const shutdown = require('./src/util/shutdown.util');
-const { SERVER } = require('./src/constants');
 
 module.exports.start = async () => {
   storage.setup();
