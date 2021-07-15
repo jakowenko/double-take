2b6bf45577c59890e756bacf96747389457ccc74
David Jakowenko <jakowenko@gmail.com>
2021-07-15 02:42:21 -0400

    fix: use exec command when starting api
    this allows the api to listen for shutdown signals

--- .develop/entrypoint.dev.sh
+++ .develop/entrypoint.dev.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ./.storage/config
 [ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml
-npm run api
+exec npm run api
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ./.storage/config
 [ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml
-nodemon -e yml --watch ./.storage/config --watch ./config.yml -q api/server.js
+exec nodemon -e yml --watch ./.storage/config --watch ./config.yml -q api/server.js
