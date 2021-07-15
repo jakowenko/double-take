8b17248f0de31ac4acce608875c2878d1d921b74
David Jakowenko <jakowenko@gmail.com>
2021-07-14 23:19:02 -0400

    chore: update tile on ui routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,48 @@
 import { /* createWebHistory, */ createWebHashHistory, createRouter } from 'vue-router';
 import Match from '@/views/Match.vue';
 import Config from '@/views/Config.vue';
-import Files from '@/views/Files.vue';
+import Train from '@/views/Train.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Match',
+    meta: {
+      title: 'Matches',
+    },
     component: Match,
   },
   {
     path: '/config',
-    name: 'Config',
+    meta: {
+      title: 'Config',
+    },
     component: Config,
   },
   {
-    path: '/files',
-    name: 'Files',
-    component: Files,
+    path: '/train',
+    meta: {
+      title: 'Train',
+    },
+    component: Train,
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/',
   },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior() {
+    // Scroll to the top of the page on route navigation
+    return { x: 0, y: 0 };
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title ? `${to.meta.title} | Double Take` : 'Double Take';
+  next();
 });
 
 export default router;
