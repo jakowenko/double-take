7395e666cf53ffd28c00b93d88d8e15fdc6396ad
David Jakowenko <jakowenko@gmail.com>
2021-07-30 03:18:34 -0400

    chore(api): update folders function to object with methods

--- api/src/controllers/fs.controller.js
+++ api/src/controllers/fs.controller.js
@@ -4,26 +4,24 @@ const { respond, HTTPSuccess } = require('../util/respond.util');
 const { OK } = require('../constants/http-status');
 const { STORAGE } = require('../constants');
 
-module.exports.folders = () => {
-  return {
-    list: async (req, res) => {
-      try {
-        const folders = await filesystem.folders().train();
-        respond(HTTPSuccess(OK, folders), res);
-      } catch (error) {
-        respond(error, res);
+module.exports.folders = {
+  list: async (req, res) => {
+    try {
+      const folders = await filesystem.folders().train();
+      respond(HTTPSuccess(OK, folders), res);
+    } catch (error) {
+      respond(error, res);
+    }
+  },
+  create: (req, res) => {
+    try {
+      const { name } = req.params;
+      if (!fs.existsSync(`${STORAGE.PATH}/train/${name}`)) {
+        fs.mkdirSync(`${STORAGE.PATH}/train/${name}`);
       }
-    },
-    create: (req, res) => {
-      try {
-        const { name } = req.params;
-        if (!fs.existsSync(`${STORAGE.PATH}/train/${name}`)) {
-          fs.mkdirSync(`${STORAGE.PATH}/train/${name}`);
-        }
-        respond(HTTPSuccess(OK, { success: true }), res);
-      } catch (error) {
-        respond(error, res);
-      }
-    },
-  };
+      respond(HTTPSuccess(OK, { success: true }), res);
+    } catch (error) {
+      respond(error, res);
+    }
+  },
 };
