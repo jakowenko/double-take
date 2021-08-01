6f12672614fe63cb6f6a19f1f16360989e1e1dee
David Jakowenko <jakowenko@gmail.com>
2021-08-01 02:11:17 -0400

    refactor(ui:login): updates
    - emit if auth is enabled
    - push to / if valid JWT

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -29,7 +29,14 @@
           :disabled="isDisabled"
         />
       </div>
-      <p v-if="firstSetup">Please set a password</p>
+      <div v-if="firstSetup">
+        <p>Please set a password</p>
+        <hr />
+        <p>
+          To disable authentication:<br />Add <span class="code">auth:</span> <span class="code">false</span> to your
+          <span class="code">config.yml</span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -65,14 +72,16 @@ export default {
   methods: {
     async setup() {
       try {
-        if (localStorage.getItem('token')) {
-          this.$router.push('/');
-          return;
-        }
         this.loading = true;
         await Sleep(250);
         const { data } = await ApiService.get('auth/status');
+        this.emitter.emit('hasAuth', data.auth);
         if (data.auth === false) {
+          this.$router.push('/');
+          return;
+        }
+
+        if (data.jwtValid) {
           this.$router.push('/');
           return;
         }
@@ -89,12 +98,12 @@ export default {
         this.loading = true;
         await Sleep(250);
         const { data } = this.firstSetup
-          ? await ApiService.patch('auth/password', { password: this.password })
+          ? await ApiService.post('auth/password', { password: this.password })
           : await ApiService.post('auth', { password: this.password });
         localStorage.setItem('token', data.token);
         this.$router.push('/');
       } catch (error) {
-        error.message = error.response && error.response.status === 401 ? 'Invalid Password' : error.message;
+        error.message = error.response && error.response.status === 401 ? 'Password Incorrect' : error.message;
         this.emitter.emit('error', error);
       }
       this.loading = false;
@@ -117,6 +126,16 @@ export default {
   background: var(--surface-a);
   padding: 2rem;
   border-radius: 5px;
+  max-width: 350px;
+  hr {
+    border-style: none;
+    border-top: 1px solid var(--surface-d);
+    border-bottom: 0;
+  }
+  .code {
+    font-family: monospace;
+    font-weight: bold;
+  }
 }
 
 .p-field,
