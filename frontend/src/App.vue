58298e1b4ab7881b1d73a20b9c8b092a7afde6bf
David Jakowenko <jakowenko@gmail.com>
2021-08-01 02:51:17 -0400

    refactor: listen for emits to handle login, errors, and messages
    - minor css updates to handle menu items

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import ApiService from '@/services/api.service';
 import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeflex/primeflex.css';
@@ -24,8 +25,44 @@ export default {
     ConfirmDialog,
     Toolbar,
   },
+  created() {
+    window.addEventListener('focus', this.checkLoginState);
+    this.emitter.on('login', this.login);
     this.emitter.on('error', (error) => this.error(error));
     this.emitter.on('toast', (...args) => this.toast(...args));
+  },
+  mounted() {
+    this.checkLoginState();
+  },
+  methods: {
+    login() {
+      if (this.$route.path !== '/login') {
+        this.$router.push('/logout');
+      }
+    },
+    error(error) {
+      if (error.response && error.response.config.url !== 'auth' && error.response.status === 401) return;
+      this.$toast.add({
+        severity: 'error',
+        detail: error.message,
+        life: 3000,
+      });
+    },
+    toast(opts = {}) {
+      this.$toast.add({
+        severity: opts.severity || 'success',
+        detail: opts.message || 'Success',
+        life: 3000,
+      });
+    },
+    async checkLoginState() {
+      const { data } = await ApiService.get('auth/status');
+      this.emitter.emit('hasAuth', data.auth);
+      if (data.auth && !data.jwtValid) {
+        this.$router.push('login');
+      }
+    },
+  },
 };
 </script>
 
@@ -56,6 +93,39 @@ body {
 
 .p-toast .p-toast-detail {
   margin-top: 0 !important;
+}
+
+.p-tooltip .p-tooltip-text {
+  font-size: 0.75rem;
+}
+.p-tooltip-left {
+  .p-tooltip-arrow {
+    margin-right: 5px;
+  }
+  .p-tooltip-text {
+    margin-right: 5px;
+    padding: 0.25rem;
+  }
+}
+
+.double-take-menu {
+  font-size: 0.9rem;
+  width: 175px;
+  margin-top: 2px;
+
+  .p-submenu-header,
+  a.p-menuitem-link {
+    padding: 0.5rem 1rem;
+  }
+
+  .p-submenu-header {
+    display: none;
+  }
+
+  .p-menuitem:nth-child(2) a {
+    font-weight: bold;
+    font-size: 0.75rem;
+  }
 }
 
 @media only screen and (max-width: 576px) {
