d3a55a68585569a07d7dd368732ec5678b3ccb23
David Jakowenko <jakowenko@gmail.com>
2021-08-01 16:36:18 -0400

    refactor(api): handle ui web history with routes

--- api/src/app.js
+++ api/src/app.js
@@ -6,8 +6,17 @@ const app = express();
 app.use('*', cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  express.static(process.env.NODE_ENV === 'development' ? './frontend/dist/' : './frontend/')
+);
 app.use('/api', router);
 app.use('/api/tmp', express.static(`/tmp`));
-app.use('/', express.static(`./frontend`));
+app.use('/', (req, res) => {
+  res.sendFile(
+    process.env.NODE_ENV === 'development'
+      ? `${process.cwd()}/frontend/dist/index.html`
+      : `${process.cwd()}/frontend/index.html`
+  );
+});
 
 module.exports = { app };
