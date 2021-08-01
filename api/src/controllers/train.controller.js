80f21e60653ca20e89f4b06ad27bb61dd72cffc0
David Jakowenko <jakowenko@gmail.com>
2021-08-01 02:46:00 -0400

    refactor: get ids from body directly

--- api/src/controllers/train.controller.js
+++ api/src/controllers/train.controller.js
@@ -73,7 +73,7 @@ module.exports.get = async (req, res) => {
 module.exports.delete = async (req, res) => {
   try {
     const { name } = req.params;
-    const { ids } = req.body;
+    const ids = req.body;
     const results = await train.remove(name, { ids });
 
     respond(HTTPSuccess(OK, results), res);
