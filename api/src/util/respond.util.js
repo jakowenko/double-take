77197cdf008f13a218bd7535452a25201760f1a1
David Jakowenko <jakowenko@gmail.com>
2021-08-05 01:34:13 -0400

    refactor: better logging of errors in respond util

--- api/src/util/respond.util.js
+++ api/src/util/respond.util.js
@@ -1,29 +1,21 @@
 const { BAD_REQUEST } = require('../constants/http-status');
 
-module.exports.respond = (err, res) => {
+module.exports.respond = (input, res) => {
   try {
-    const status = err && err.status ? err.status : BAD_REQUEST;
+    const isError = input instanceof Error;
+    const status = input && input.status ? input.status : BAD_REQUEST;
     const firstChar = parseInt(status.toString().charAt(0), 10);
     let message = { success: true };
 
-    if (firstChar === 4 || firstChar === 5) {
-      message = { error: err.message };
-    }
+    if (firstChar === 4 || firstChar === 5) message = { error: input.message };
 
-    if (firstChar === 2) {
-      message = err.message ? err.message : message;
-    }
+    if (firstChar === 2) message = input.message ? input.message : message;
 
-    if (message.error) {
-      if (message.error.includes('warn:')) {
-        message.error = message.error.replace('warn:', '').trim();
-        console.warn(message.error);
-      } else console.error(message.error);
-    }
+    if (isError && !input.http) console.error(input);
 
     return res.status(status).json(message);
   } catch (error) {
-    console.error(err || error);
+    console.error(input || error);
   }
 };
 
@@ -32,15 +24,20 @@ module.exports.HTTPSuccess = (status, message) => {
 };
 
 module.exports.HTTPError = (status, message) => {
+  if (message.includes('warn:')) {
+    message = message.replace('warn:', '').trim();
+    console.warn(message);
+  } else console.error(message);
+
   if (typeof message === 'string') {
     const error = new Error(message);
     error.status = status;
+    error.http = true;
     return error;
   }
   if (typeof message === 'object') {
-    return { status, message };
+    return { status, message, http: true };
   }
 
-  return { status, message: true };
+  return { status, message: true, http: true };
 };
-// this.HTTPError.prototype = Object.create(Error.prototype);
