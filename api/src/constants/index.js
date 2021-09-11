eeb7852d93da5629237353bfbca65ad8ac689051
David Jakowenko <jakowenko@gmail.com>
2021-09-11 01:11:10 -0400

    chore: move objectKeysToUpperCase to own util function

--- api/src/constants/index.js
+++ api/src/constants/index.js
@@ -1,15 +1,4 @@
 const config = require('./config');
-
-const objectKeysToUpperCase = (input) => {
-  if (input === null) return input;
-  if (typeof input !== 'object') return input;
-  if (Array.isArray(input)) return input.map(objectKeysToUpperCase);
-  return Object.keys(input).reduce((newObj, key) => {
-    const val = input[key];
-    const newVal = typeof val === 'object' ? objectKeysToUpperCase(val) : val;
-    newObj[key.toUpperCase()] = newVal;
-    return newObj;
-  }, {});
-};
+const { objectKeysToUpperCase } = require('../util/object.util');
 
 module.exports = objectKeysToUpperCase(config());
