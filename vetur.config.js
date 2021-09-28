df6f343c36c4662d3d8501578669617adc6187af
David Jakowenko <jakowenko@gmail.com>
2021-09-28 00:24:18 -0400

    style: linting

--- vetur.config.js
+++ vetur.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    projects: [
-      {
-        root: './frontend',
-        package: './package.json',
-        jsconfig: './jsconfig.json'
-      }
-    ]
-  }
+  projects: [
+    {
+      root: './frontend',
+      package: './package.json',
+      jsconfig: './jsconfig.json',
+    },
+  ],
+};
