2346f9aca3581edcc02c030564a01420770e7137
David Jakowenko <jakowenko@gmail.com>
2021-10-27 01:34:26 -0400

    chore: hass.io add-on entrypoint

--- .build/entrypoint.sh
+++ .build/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-mkdir -p ./.storage/config
-[ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml
-exec nodemon -e yml --watch ./.storage/config --watch ./config.yml -q api/server.js
+if [ "$CONFIG_PATH" ]
+then
+  mkdir -p "$CONFIG_PATH"
+  [ -f "$CONFIG_PATH"/config.yml ] || echo $'# Double Take' > "$CONFIG_PATH"/config.yml
+  exec nodemon -e yml --watch "$CONFIG_PATH" -q api/server.js
+else
+  mkdir -p ./.storage/config
+  [ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml
+  exec nodemon -e yml --watch ./.storage/config --watch ./config.yml -q api/server.js
+fi
