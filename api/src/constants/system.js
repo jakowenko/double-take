f0e321ab67f7befbe10f4faf672b889ba91fb1c4
David Jakowenko <jakowenko@gmail.com>
2021-11-26 13:32:38 -0500

    chore: adjust secrets path through env var

--- api/src/constants/system.js
+++ api/src/constants/system.js
@@ -5,6 +5,7 @@ module.exports.core = {
   storage: {
     path: process.env.STORAGE_PATH ? process.env.STORAGE_PATH : './.storage',
     config: { path: process.env.CONFIG_PATH ? process.env.CONFIG_PATH : './.storage/config' },
+    secrets: { path: process.env.SECRETS_PATH ? process.env.SECRETS_PATH : './.storage/config' },
     media: { path: process.env.MEDIA_PATH ? process.env.MEDIA_PATH : './.storage' },
     tmp: { path: fs.existsSync('/dev/shm') ? '/dev/shm/double-take' : '/tmp/double-take' },
   },
