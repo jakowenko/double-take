9b4ac42d5c32f0193214b1d06ffcda4c1aeaaf71
David Jakowenko <jakowenko@gmail.com>
2021-11-26 13:31:27 -0500

    refactor: let api write config/secrets.yml files

--- .build/entrypoint.sh
+++ .build/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+PATHS="";
 if [ "$CONFIG_PATH" ]
 then
-  mkdir -p "$CONFIG_PATH"
-  [ -f "$CONFIG_PATH"/config.yml ] || echo $'# Double Take' > "$CONFIG_PATH"/config.yml
-  exec nodemon -e yml --watch "$CONFIG_PATH" -q api/server.js
+  PATHS="$PATHS --watch "$CONFIG_PATH"";
 else
-  mkdir -p ./.storage/config
-  [ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml
-  exec nodemon -e yml --watch ./.storage/config --watch ./config.yml -q api/server.js
+  PATHS="$PATHS --watch ./.storage/config";
 fi
+
+if [ "$SECRETS_PATH" ]
+then
+  PATHS="$PATHS --watch "$SECRETS_PATH"";
+elif [ "$CONFIG_PATH" ]
+then
+  PATHS="$PATHS --watch ./.storage/config";
+fi
+
+exec nodemon -e yml $PATHS -q api/server.js
--- .develop/entrypoint.dev.sh
+++ .develop/entrypoint.dev.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-mkdir -p ./.storage/config
-[ -f ./.storage/config/config.yml ] || echo $'# Double Take' > ./.storage/config/config.yml
 exec npm run api
