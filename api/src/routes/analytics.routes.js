1ab9f771cdaea55d7192dff74d05dfb4d81b027a
David Jakowenko <jakowenko@gmail.com>
2022-05-27 01:32:45 -0400

    refactor(api):  remove analytics routes

--- api/src/routes/analytics.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { analytics } = require('../controllers/analytics.controller');
-
-const router = express.Router();
-
-router.get('/', analytics);
-
-module.exports = router;
