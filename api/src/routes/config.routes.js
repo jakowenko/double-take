9c19f7923d632976fa33e8214f1b1b5147204c69
David Jakowenko <jakowenko@gmail.com>
2022-06-12 00:34:13 -0400

    refactor(api): joi validation

--- api/src/routes/config.routes.js
+++ api/src/routes/config.routes.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const { jwt, validate, expressValidator } = require('../middlewares');
+const { jwt, validate, Joi } = require('../middlewares');
 const controller = require('../controllers/config.controller');
-
-const { query } = expressValidator;
 
 const router = express.Router();
 
@@ -10,13 +8,25 @@ router
   .get(
     '/',
     jwt,
-    validate([query('format').default('json').isIn(['json', 'yaml', 'yaml-with-defaults'])]),
+    validate({
+      query: { format: Joi.string().valid('json', 'yaml', 'yaml-with-defaults').default('json') },
+    }),
     controller.get
   )
-  .patch('/', jwt, controller.patch)
+  .patch('/', jwt, validate({ body: { code: Joi.string().min(0).required() } }), controller.patch)
   .get('/theme', controller.theme.get)
-  .patch('/theme', jwt, controller.theme.patch)
+  .patch(
+    '/theme',
+    jwt,
+    validate({ body: { ui: Joi.string().required(), editor: Joi.string().required() } }),
+    controller.theme.patch
+  )
   .get('/secrets', jwt, controller.secrets.get)
-  .patch('/secrets', jwt, controller.secrets.patch);
+  .patch(
+    '/secrets',
+    jwt,
+    validate({ body: { code: Joi.string().min(0).required() } }),
+    controller.secrets.patch
+  );
 
 module.exports = router;
