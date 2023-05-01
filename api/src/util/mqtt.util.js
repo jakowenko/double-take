7c36ae1e5791bbb441219f4c5a21f41932d80ac1
Sergey Krashevich <svk@svk.su>
2023-05-01 15:46:30 +0300

    Add device trackers for recognized people and set their state to "home", with a timeout to set them to "not_home" after 30 minutes

--- api/src/util/mqtt.util.js
+++ api/src/util/mqtt.util.js
@@ -248,6 +248,33 @@ module.exports.recognize = (data) => {
       });
 
       if (MQTT.TOPICS.HOMEASSISTANT) {
+        // Add device tracker for each recognized person
+        messages.push({
+          topic: `${MQTT.TOPICS.HOMEASSISTANT}/device_tracker/double-take/${topic}/config`,
+          retain: true,
+          message: JSON.stringify({
+            name: `${match.name}`,
+            object_id: `double_take_${name}`,
+            icon: 'mdi:account',
+            state_topic: `${MQTT.TOPICS.HOMEASSISTANT}/device_tracker/double-take/${topic}/state`,
+            unique_id: `double_take_tracker_${name}`,
+          }),
+        });
+
+        messages.push({
+          topic: `${MQTT.TOPICS.HOMEASSISTANT}/device_tracker/double-take/${topic}/state`,
+          retain: true,
+          message: 'home',
+        });
+        clearTimeout(PERSON_RESET_TIMEOUT[topic]);
+        PERSON_RESET_TIMEOUT[topic] = setTimeout(() => {
+          this.publish({
+            topic: `${MQTT.TOPICS.HOMEASSISTANT}/device_tracker/double-take/${topic}/state`,
+            retain: true,
+            message: 'not_home',
+          });
+        }, 1000 * 60 * 30); // 30 min
+
         messages.push({
           topic: `${MQTT.TOPICS.HOMEASSISTANT}/sensor/double-take/${topic}/config`,
           retain: true,
