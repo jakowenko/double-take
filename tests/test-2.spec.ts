c91561f507dffbc6040d0f1b2d14d8068758d7c1
Sergey Krashevich <svk@svk.su>
2023-11-20 00:18:14 +0300

    Add Playwright Tests and Update Dependencies
    This commit introduces Playwright tests for the application. A new GitHub workflow has been added to run these tests on push and pull request events on the main and master branches. The tests are configured to run on the latest Ubuntu environment and use Node version 18.
    
    The Playwright configuration file and test specifications have also been added. The tests are designed to run on Chromium, Firefox, and Webkit browsers.
    
    In addition, this commit updates the .gitignore file to ignore test results and Playwright cache.

---
+++ tests/test-2.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('pages', async ({ page }) => {
+  await page.goto('./');
+  await page.getByLabel('Matches').click();
+  await expect(page.locator('.match-wrapper')).toBeVisible();
+  await page.getByLabel('Train').click();
+  await expect(page.locator('.train-wrapper')).toBeVisible();
+  await page.getByLabel('Config').click();
+  await expect(page.locator('.ace_content')).toBeVisible({timeout: 15000});
+  await page.getByLabel('Logs').click();
+  await expect(page.locator('pre')).toBeVisible();
+});
+test('right-menu', async ({ page }) => {
+  await page.goto('./');
+  await page.getByText('Double Take').click();
+  await expect(page.getByLabel('v1.13.11.7').locator('a')).toBeVisible();
+  await page.locator('#pv_id_1_0_2 a').click();
+  await page.getByText('Double Take').click();
+  await expect(page.locator('#pv_id_1_0_2 a')).toBeVisible();
+});
