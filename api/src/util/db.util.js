bd7abb5a7d0acf86a0b592ef04a23ecdd7ef36f1
Sergey Krashevich <svk@svk.su>
2023-12-05 13:01:56 +0300

    Refactored database utility functions

--- api/src/util/db.util.js
+++ api/src/util/db.util.js
@@ -14,12 +14,70 @@ function connect() {
     });
   return connection;
 }
+function createFile({ name, filename, meta }) {
+  const db = connect();
+  db.prepare(
+    `INSERT INTO file (name, filename, meta, isActive, createdAt)
+        VALUES (:name, :filename, :meta, :isActive, :createdAt)
+        ON CONFLICT (name, filename) DO UPDATE SET isActive = 1;`
+  ).run({
+    name,
+    filename,
+    meta: meta || null,
+    createdAt: time.utc(),
+    isActive: 1,
+  });
+}
+async function resync() {
+  const db = connect();
+  db.prepare(`UPDATE file SET isActive = 0`).run();
+  const files = await filesystem.files.train();
+  files.forEach((obj) => createFile(obj));
+}
+
+function migrations() {
+  const db = connect();
+
+  const matchTableInfo = db.prepare('PRAGMA table_info(match)').all();
+  const fileTableInfo = db.prepare('PRAGMA table_info(file)').all();
+
+  if (!matchTableInfo.some((obj) => obj.name === 'filename')) {
+    db.prepare('DROP TABLE IF EXISTS match').run();
+  }
+
+  if (fileTableInfo.some((obj) => obj.name === 'uuid')) {
+    db.transaction(() => {
+      db.prepare('ALTER TABLE file RENAME TO file_backup').run();
+
+      db.prepare(
+        `
+        CREATE TABLE file (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT,
+          filename TEXT,
+          meta JSON,
+          isActive INTEGER,
+          createdAt TIMESTAMP,
+          UNIQUE(name, filename)
+        )
+      `
+      ).run();
+
+      db.prepare(
+        `
+        INSERT INTO file (id, name, filename, meta, isActive, createdAt)
+        SELECT id, name, filename, meta, isActive, createdAt FROM file_backup
+      `
+      ).run();
+
+      db.prepare('DROP TABLE file_backup').run();
+    })();
+  }
+}
 
 async function init() {
   try {
     const db = connect();
-
-    migrations();
 
     db.prepare(
       `CREATE TABLE IF NOT EXISTS file (
@@ -65,66 +123,17 @@ async function init() {
 
     db.prepare(`DELETE FROM train WHERE meta IS NULL`).run();
 
+    migrations();
+
     await resync();
   } catch (error) {
     error.message = `db init error: ${error.message}`;
     console.error(error);
   }
 }
-
-function migrations() {
-  try {
-    const db = connect();
-
-    if (
-      !db
-        .prepare('PRAGMA table_info(match)')
-        .all()
-        .filter((obj) => obj.name === 'filename').length
-    ) {
-      db.prepare('DROP TABLE IF EXISTS match').run();
-    }
-
-    if (
-      db
-        .prepare('PRAGMA table_info(file)')
-        .all()
-        .filter((obj) => obj.name === 'uuid').length
-    ) {
-      db.exec(
-        `
-          BEGIN TRANSACTION;
-          CREATE TEMPORARY TABLE file_backup (id, name, filename, meta, isActive, createdAt);
-          INSERT INTO file_backup SELECT id, name, filename, meta, isActive, createdAt FROM file;
-          DROP TABLE file;
-          CREATE TABLE file (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name,
-            filename,
-            meta JSON,
-            isActive INTEGER,
-            createdAt TIMESTAMP,
-            UNIQUE(name, filename)
-          );
-          INSERT INTO file SELECT id, name, filename, meta, isActive, createdAt FROM file_backup;
-          DROP TABLE file_backup;
-          COMMIT;
-      `
-      );
-    }
-  } catch (error) {
-    error.message = `db migrations error: ${error.message}`;
-    console.error(error);
-  }
+function params(array) {
+  return '?,'.repeat(array.length).slice(0, -1);
 }
-
-async function resync() {
-  const db = connect();
-  db.prepare(`UPDATE file SET isActive = 0`).run();
-  const files = await filesystem.files.train();
-  files.forEach((obj) => createFile(obj));
-}
-
 function getUntrained(name) {
   const db = connect();
   return db
@@ -151,21 +160,6 @@ function getFileByFilename(name, filename) {
   return file || false;
 }
 
-function createFile({ name, filename, meta }) {
-  const db = connect();
-  db.prepare(
-    `INSERT INTO file
-        VALUES (:id, :name, :filename, :meta, :isActive, :createdAt)
-        ON CONFLICT (name, filename) DO UPDATE SET isActive = 1;`
-  ).run({
-    id: null,
-    name,
-    filename,
-    meta: meta || null,
-    createdAt: time.utc(),
-    isActive: 1,
-  });
-}
 function createTrain({ id, name, filename, detector, meta }) {
   const db = connect();
   db.prepare(
@@ -203,10 +197,6 @@ function updateMatch({ id, event, response }) {
     response: response ? JSON.stringify(response) : null,
     id,
   });
-}
-
-function params(array) {
-  return '?,'.repeat(array.length).slice(0, -1);
 }
 
 module.exports = {
