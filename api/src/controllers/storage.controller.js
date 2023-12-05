89471c6c7a6bcf6b4a515ee0e15b7ae9d0f8079f
Sergey Krashevich <svk@svk.su>
2023-12-06 02:47:12 +0300

    fix potential  path-injection vuln

--- api/src/controllers/storage.controller.js
+++ api/src/controllers/storage.controller.js
@@ -10,10 +10,11 @@ const { BAD_REQUEST } = require('../constants/http-status');
 const { AUTH, SERVER, UI } = require('../constants')();
 const { PATH } = require('../constants')().STORAGE.MEDIA;
 const { QUALITY, WIDTH } = require('../constants')().UI.THUMBNAILS;
+const sanitize = require('sanitize-filename-truncate');
 
 module.exports.matches = async (req, res) => {
   const { box: showBox } = req.query;
-  const { filename } = req.params;
+  const { filename } = sanitize(req.params);
   const source = `${PATH}/matches/${filename}`;
 
   if (!fs.existsSync(source)) {
