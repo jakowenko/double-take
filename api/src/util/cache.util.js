4662df3f3cadfce2712e5b1d3fc0f615ef4a12bc
Sergey Krashevich <svk@svk.su>
2023-12-06 10:07:01 +0300

    Refactor cache.util.js to include verbose logging for cache get and set operations

--- api/src/util/cache.util.js
+++ api/src/util/cache.util.js
@@ -3,6 +3,12 @@ const NodeCache = require('node-cache');
 const DTCache = new NodeCache();
 
 module.exports = {
-  get: (key) => DTCache.get(key),
-  set: (key, value, ttl) => DTCache.set(key, value, ttl),
+  get: (key) => {
+    console.verbose(`Cache get: ${key}`);
+    return DTCache.get(key);
+  },
+  set: (key, value, ttl) => {
+    console.verbose(`Cache set: ${key}, ttl: ${ttl}`);
+    return DTCache.set(key, value, ttl);
+  },
 };
