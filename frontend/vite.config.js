4e6dda548c6c46b0cdad4c1c9d412c5b442b7e57
Sergey Krashevich <svk@svk.su>
2023-12-06 10:02:56 +0300

    Add relative-ci workflow and artifact upload action

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import commonjsExternals from 'vite-plugin-commonjs-externals';
 // import svgLoader from 'vite-svg-loader'
+import { webpackStats } from 'rollup-plugin-webpack-stats';
 
 // https://vitejs.dev/config/
 
@@ -16,17 +17,26 @@ export default defineConfig(({ command }) => ({
     commonjsExternals({
       externals,
     }),
-          ],
-        base: command === 'serve' ? '/' : './',
+    // Output webpack-stats.json file
+    webpackStats(),
+  ],
+  base: command === 'serve' ? '/' : './',
 
-        resolve: {
-            alias: {
+  resolve: {
+    alias: {
       '@': path.resolve(__dirname, './src'),
-            },
+    },
     // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-        },
-        build: {
+  },
+  build: {
     outDir: 'dist',
-
+    rollupOptions: {
+      output: {
+        // Use a supported file pattern for Vite 5/Rollup 4
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
 }));
