a94f296ac28ce0dc6339d05e43653852bc643bee
Sergey Krashevich <svk@svk.su>
2023-12-06 07:37:52 +0300

    Add object-hash library for caching filters and total count in match controller

--- api/src/controllers/match.controller.js
+++ api/src/controllers/match.controller.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const sizeOf = require('probe-image-size');
-const crypto = require('crypto');
 const database = require('../util/db.util');
 const filesystem = require('../util/fs.util');
 const { tryParseJSON } = require('../util/validators.util');
@@ -10,6 +9,7 @@ const { AUTH, STORAGE, UI } = require('../constants')();
 const { BAD_REQUEST } = require('../constants/http-status');
 const DETECTORS = require('../constants/config').detectors();
 const Cache = require('../util/cache.util');
+const objhasher = require('object-hash');
 
 const format = async (matches) => {
   const token = AUTH && matches.length ? jwt.sign({ route: 'storage' }) : null;
@@ -77,30 +77,36 @@ module.exports.post = async (req, res) => {
   const confidenceQuery =
     filters.confidence === 0 ? `OR json_extract(value, '$.confidence') IS NULL` : '';
 
-  const [total] = db
-    .prepare(
-      `SELECT COUNT(id) count FROM match_extended
-    WHERE json_extract(value, '$.name') IN (${database.params(filters.names)})
-    AND json_extract(value, '$.match') IN (${database.params(filters.matches)})
-    AND json_extract(event, '$.camera') IN (${database.params(filters.cameras)})
-    AND json_extract(event, '$.type') IN (${database.params(filters.types)})
-    AND (json_extract(value, '$.confidence') >= ? ${confidenceQuery})
-    AND json_extract(value, '$.box.width') >= ?
-    AND json_extract(value, '$.box.height') >= ?
-    AND detector IN (${database.params(filters.detectors)})`
-    )
-    .bind(
-      filters.names,
-      filters.matches.map((obj) => (obj === 'match' ? 1 : 0)),
-      filters.cameras,
-      filters.types,
-      filters.confidence,
-      filters.width,
-      filters.height,
-      filters.detectors
-    )
-    .all();
-
+  const filtersHash = objhasher(filters);
+  let total;
+  if (Cache.get(filtersHash)) {
+    total = Cache.get(filtersHash);
+  } else {
+    [total] = db
+      .prepare(
+        `SELECT COUNT(id) count FROM match_extended
+      WHERE json_extract(value, '$.name') IN (${database.params(filters.names)})
+      AND json_extract(value, '$.match') IN (${database.params(filters.matches)})
+      AND json_extract(event, '$.camera') IN (${database.params(filters.cameras)})
+      AND json_extract(event, '$.type') IN (${database.params(filters.types)})
+      AND (json_extract(value, '$.confidence') >= ? ${confidenceQuery})
+      AND json_extract(value, '$.box.width') >= ?
+      AND json_extract(value, '$.box.height') >= ?
+      AND detector IN (${database.params(filters.detectors)})`
+      )
+      .bind(
+        filters.names,
+        filters.matches.map((obj) => (obj === 'match' ? 1 : 0)),
+        filters.cameras,
+        filters.types,
+        filters.confidence,
+        filters.width,
+        filters.height,
+        filters.detectors
+      )
+      .all();
+    Cache.set(filtersHash, total, 60);
+  }
   const matches = db
     .prepare(
       `SELECT me.*, train.isTrained FROM match_extended me
