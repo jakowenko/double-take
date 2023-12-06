bee91d8afd1ada8ecc2febd7397b3e2af7076bf5
Sergey Krashevich <svk@svk.su>
2023-12-06 10:02:56 +0300

    Add relative-ci workflow and artifact upload action

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import commonjsExternals from 'vite-plugin-commonjs-externals';
 // import svgLoader from 'vite-svg-loader'
 import EnvironmentPlugin from 'vite-plugin-environment';
+import { webpackStats } from 'rollup-plugin-webpack-stats';
 
 // https://vitejs.dev/config/
 
@@ -11,24 +12,33 @@ const path = require('path');
 const externals = ['path', /^src(\/.+)?$/];
 
 export default defineConfig(({ command }) => ({
-        plugins: [
-            vue(),
-    EnvironmentPlugin({DOUBLETAKE_HOST: null, DOUBLETAKE_PORT: 3000}),
+  plugins: [
+    vue(),
+    EnvironmentPlugin({ DOUBLETAKE_HOST: null, DOUBLETAKE_PORT: 3000 }),
     // svgLoader(),
     commonjsExternals({
       externals,
     }),
-          ],
-        base: command === 'serve' ? '/' : './',
+    // Output webpack-stats.json file
+    webpackStats(),
+  ],
+  base: command === 'serve' ? '/' : './',
 
-        resolve: {
-            alias: {
+  resolve: {
+    alias: {
       '@': path.resolve(__dirname, './src'),
-            },
+    },
     // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-        },
-        build: {
+  },
+  build: {
     outDir: 'dist',
-
+    rollupOptions: {
+      output: {
+        // Use a supported file pattern for Vite 5/Rollup 4
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
 }));
